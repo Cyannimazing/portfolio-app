04bac10e5a78d2e66655bddcfa28cfe234ce8607
Cyannimazing <cyrilnarvasa589@gmail.com>
2026-06-03 19:08:44 +0800

    CV

--- components/ChatWidgetScript.tsx
+++ components/ChatWidgetScript.tsx
@@ -4,9 +4,9 @@ import { useEffect } from "react";
 export function ChatWidgetScript() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://server6611.aworkinsight.dk/browser-ui/chat-widget-loader.js";
-    script.setAttribute("data-widget-id", "18cb5e6e-c542-4532-9fd8-552718823854");
-    script.setAttribute("data-api-base", "https://server6611.aworkinsight.dk/api");
+    script.src = "http://127.0.0.1:8000/browser-ui/chat-widget-loader.js";
+    script.setAttribute("data-widget-id", "af89018d-2113-411d-946a-e60a32abddbb");
+    script.setAttribute("data-api-base", "http://127.0.0.1:8000/api");
     script.async = true;
     document.body.appendChild(script);
     return () => {
